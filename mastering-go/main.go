a79c43d5918e6714d60e32eec08b8e24fb68856a
calebthewood <calebw787@gmail.com>
2023-12-24 23:27:47 -1000

    create file

--- mastering-go/main.go
+++ mastering-go/main.go
@@ -1,10 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	// print
 	fmt.Println("Hello world.")
-
 	fmt.Println("2 + 2 = ", 2+2)
+
+	// create empty file
+	myFile, es := os.Create("hello.txt")
+	if es != nil {
+		log.Fatal(es)
+	}
+	log.Println(myFile)
+	myFile.Close()
 }
