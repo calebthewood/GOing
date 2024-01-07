ffc405f2e541feb578eea0071365357725bc866d
Caleb Wood <calebw787@gmail.com>
2024-01-07 10:55:39 -1000

    bool switch

--- go-docs/tour/tour.go
+++ go-docs/tour/tour.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 	"runtime"
+	"time"
 )
 
 func main() {
@@ -96,5 +97,15 @@ func switching() {
 	default:
 		//freebsd, openbsd, plan9, windows...
 		fmt.Printf("%s. \n", os)
+	}
+	// switch w/o a condition is a boolean, so each case is a boolean expression
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
 	}
 }
