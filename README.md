41467731c1820e3719d91ce69fd7c47c8126f253
calebthewood <calebw787@gmail.com>
2024-02-08 12:37:44 -1000

    pr test

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# GOing
+# GO-ing
+### A little repo of my collected golang excursions.
+- Go Docs Tuorial
+- Mastering GoLang
+- GoLang, The Ultimate Guide
+
+# PR TEST
+Hey There
--- mastering-go/main.go
+++ mastering-go/main.go
@@ -6,7 +6,17 @@ import (
 	"os"
 )
 
+var (
+	myfile *os.FileInfo
+	es     error
+)
+
 func main() {
+	partOne()
+	partTwo()
+}
+
+func partOne() {
 	// print
 	fmt.Println("Hello world.")
 	fmt.Println("2 + 2 = ", 2+2)
@@ -18,4 +28,28 @@ func main() {
 	}
 	log.Println(myFile)
 	myFile.Close()
+}
+
+func partTwo() {
+	// Stat() func returns the file in & if there is
+	// no file, it returns error
+	myfile, es := os.Stat("hello.txt")
+	if es != nil {
+		// checking if given file exists or not
+		if os.IsNotExist(es) {
+			log.Fatal("File not Found")
+		}
+	}
+	log.Println("File Exists")
+	log.Println("File Details:")
+	log.Println("Name: ", myfile.Name())
+	log.Println("Size: ", myfile.Size())
+}
+
+func partThree() {
+	// this feels wrong, but chatGPT has assured me that it is
+	// a normal way to handle errors in Go.
+	if er := os.Mkdir("a", os.ModePerm); er != nil {
+		log.Fatal(er)
+	}
 }
